8674589e15eac9a148411962f2bdff69b6cfe08a
mabouelwafa <mohamed.abouelwafa@dialpad.com>
2023-11-08 16:52:04 -0500

    Added command line arg -p and custom port

--- simple-syslog-server.py
+++ simple-syslog-server.py
@@ -19,6 +19,7 @@
 import os
 import socketserver
 import argparse
+import socket
 
 
 class SyslogHandler(socketserver.BaseRequestHandler):
@@ -56,15 +57,32 @@ if __name__ == '__main__':
   # Parse command line arguments
   parser = argparse.ArgumentParser(description='Simple Syslog Server')
   parser.add_argument('-save-logs', action='store_true', help='Save the log messages to a file')
+  parser.add_argument('-p', action='store', type=int,
+                      help='Use a different port other than the default port 514')
   args = parser.parse_args()
 
+  # Default port
+  port = args.p if args.p else 514
+  # Local IP address
+  local_ip = ''
+
   os.system('clear')
+  # Get the machine local IP address
   try:
-    print('Syslog server is listening on default port: 514\n')
-    server = socketserver.UDPServer(('0.0.0.0', 514), SyslogHandler)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))  # connecting to a remote address
+    local_ip = s.getsockname()[0]
+    s.close()
+  except Exception as e:
+    pass
+
+  try:
+    print('Syslog server: {} listening on port: {}\n'.format(local_ip, port))
+    server = socketserver.UDPServer(('0.0.0.0', port), SyslogHandler)
     server.serve_forever(poll_interval=0.5)
-  except (IOError, SystemExit):
-    raise
+  except (IOError, SystemExit) as e:
+    print(str(e))
+
   except KeyboardInterrupt:
     # Press Ctrl+C to exit
     print('Shutting down ...')
